faff7c0e5ac017eeb6ceebc28df3caddb7dfca26
mychecknix <grueb.m@googlemail.com>
2020-11-01 23:51:06 +0100

    Initial commit

---
+++ PraktikumsaufgabeOnlineShop/PraktikumsaufgabeOnlineShop/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Dynamic;
+
+namespace PraktikumsaufgabeOnlineShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * shop rules:
+             * minimum order value >= 10.00€
+             * free shipping: order value >= 30.00€
+             * included tax for total order value 16%
+             * shipping fee 3.90€
+             */
+
+            // all variables:
+            // variable for spices of the shop
+            const double pricePepper = 5.10, pricePaprika = 3.90, priceCurry = 4.50;
+            // variable for tax
+            const double tax = 0.16;
+            // variable for shipping fee
+            double shippingFee = 3.90;
+            // variable for quantity ordered spices
+            int quantityPepper = 0, quantityPaprika = 0, quantityCurry = 0;
+            // variable for total amount of the order
+            double totalAmount = 0.0,
+                totalAmountWithShipping = 0.0,
+                totalTax = 0.0,
+                totalPepper = 0.0,
+                totalPaprika = 0.0,
+                totalCurry = 0.0;
+            // variable for adding product; default value "nein" to avoid errors
+            string yesNo = "nein";
+
+            // output shop text
+            Console.WriteLine("Herzlich Willkommen bei Scharfe Küche!\n");
+            Console.WriteLine("Wie viele Gewürze möchten Sie käufen?");
+            Console.Write($"{"Pfeffer",-10}(à {pricePepper,4:f2}€):   ");
+            // input amount of each spice
+            quantityPepper = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"{"Paprika",-10}(à {pricePaprika,4:f2}€):   ");
+            quantityPaprika = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"{"Curry",-10}(à {priceCurry,4:f2}€):   ");
+            quantityCurry = Convert.ToInt32(Console.ReadLine());
+
+            // calculate values for order confirmation
+            totalPepper = quantityPepper * pricePepper;
+            totalPaprika = quantityPaprika * pricePaprika;
+            totalCurry = quantityCurry * priceCurry;
+            totalAmount = totalPepper + totalPaprika + totalCurry;
+
+            // check if mimimum order value 
+            if (totalAmount < 10.0) // check if total amount is lower than €10.00
+            {
+                Console.WriteLine("\nIhre Bestellung erreicht nicht den Mindestbestellwert von 10,00 Euro.");
+            }
+            else
+            {
+                if (totalAmount >= 25.0 && totalAmount < 30.0) // check for free shipping
+                {
+                    // show client that only a can pepper spice is needed for free shipping
+                    Console.WriteLine($"\nIhnen fehlen noch {30 - totalAmount} Euro bis zum kostenfreien Versand!");
+                    Console.WriteLine("Wollen Sie noch eine Dose Pfeffer mehr bestellen und den Versand");
+                    Console.WriteLine("sparen (ja|nein) ?"); // manual line break for better readability
+                    yesNo = Convert.ToString(Console.ReadLine());
+
+                    // check answer; all other answers are the same as "nein"
+                    if (yesNo == "ja" || yesNo == "Ja" || yesNo == "JA" || yesNo == "jA")
+                    {
+                        quantityPepper += 1;
+                        totalPepper += pricePepper;
+                        totalAmount += pricePepper;
+                    }
+                }
+
+                if (totalAmount >= 30.0) // set shipping fee to zero if total amount is equal or bigger than 30.0
+                {
+                    shippingFee = 0.0;
+                }
+
+                totalTax = totalAmount / (1 + tax) * tax;
+                totalAmountWithShipping = totalAmount + shippingFee;
+
+                // formated output oder confirmation
+                Console.WriteLine("\nBestellbestätigung durch Scharfe Küche:");
+                Console.WriteLine($"{"Pfeffer",-30}{quantityPepper,3} x {pricePepper,4:f2}{totalPepper,13:f2} EUR");
+                Console.WriteLine($"{"Paprika",-30}{quantityPaprika,3} x {pricePaprika,4:f2}{totalPaprika,13:f2} EUR");
+                Console.WriteLine($"{"Cury",-30}{quantityCurry,3} x {priceCurry,4:f2}{totalCurry,13:f2} EUR");
+                Console.WriteLine("------------------------------------------------------------------");
+                Console.WriteLine($"{"Zwischensumme",-45}{totalAmount,8:f2} EUR");
+                Console.WriteLine($"{"Enthaltene MwST (16%)",-45}{totalTax,8:f2} EUR");
+                Console.WriteLine($"{"Versandpauschale",-45}{shippingFee,8:f2} EUR");
+                Console.WriteLine("------------------------------------------------------------------");
+                Console.WriteLine($"{"Summe",-45}{totalAmountWithShipping,8:f2} EUR");
+            }
+        }
+    }
+}
---
+++ Schleifen/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Schleifen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // START task a)
+            int inNumber;    // integer variable for entered number
+            bool numCheck = false; // flag for input number status
+
+            do
+            {
+                Console.Write("Geben Sie eine Zahl größer gleich 10 und kleiner als 10000 ein: ");
+                inNumber = Convert.ToInt32(Console.ReadLine());
+                // check the number and set a flag
+                if (inNumber < 10 || inNumber >= 10000)
+                {
+                    Console.WriteLine("Ungültige Eingabe, bitte wiederholen.");
+                } else
+                {
+                    numCheck = true;
+                }
+                Console.WriteLine();    // empty line to separate output
+            } while (!numCheck);    // loop will run unt
+            // END task a)
+
+            // START task b)
+            if (inNumber < 1000)
+            {
+                Console.WriteLine($"50-er kleiner als {inNumber}:");
+                if (inNumber <= 50)
+                {
+                    Console.WriteLine($"Es gibt keine 50-er kleiner als {inNumber}");
+                }
+                else
+                {
+                    // subtract 1 from the number if it is exactly divisible by 50 or 500
+                    // this corrects the counter variable i by 1, because we only want numbers lower than 50 or 500
+                    // (inNumber - 1) / 50 to find the biggest "50-er" and than count down
+                    for (int i = (inNumber - 1) / 50; i > 0; i--)
+                    {
+                        Console.WriteLine(i*50);
+                    }
+                }
+                
+            } else
+            {
+                Console.WriteLine($"500-er kleiner als {inNumber}:");
+                for (int i = (inNumber - 1) / 500; i > 0; i--)
+                {
+                    Console.WriteLine(i * 500);
+                }
+            }
+            Console.WriteLine();    // empty line to separate output
+            // END task b)
+
+            // START task c)
+            int sumDivider = 0;
+            Console.WriteLine($"Teier von {inNumber}:");
+            // find all divider from inNumber-1 to 2
+            for (int i = (inNumber-1); i >= 2; i--)
+            {
+                // it is a real divider if rest of the modulo operation is zero
+                if(inNumber % i == 0)
+                {
+                    Console.WriteLine(inNumber / i);
+                    sumDivider += inNumber / i; // add divider to the sum of dividers
+                }
+            }
+            Console.WriteLine($"Die Summe aller echten Teiler von {inNumber} ist {sumDivider}.");
+            Console.WriteLine();
+            // END task c)
+
+            // START task d)
+            int countEven = 0;
+            int countOdd = 0;
+            // check every number for even or odd until counter is bigger than 10 times the number
+            for (int i = 10; i <= (inNumber * 10);  i *= 10)
+            {
+                // get rest of current position and the position after it and divide it by 10^(n-1) to get only the current position
+                if(((inNumber % i) / (i / 10)) % 2 == 0)
+                {
+                    countEven++;    // add one to this counter if it is even
+                } else
+                {
+                    countOdd++;     // add one to this counter if it is odd 
+                }
+            }
+            Console.WriteLine($"Die Zahl {inNumber} hat {countEven} gerade und {countOdd} ungerade Ziffern.");
+            Console.WriteLine();
+            // END task d)
+
+            // START task e)
+            int firstDigit = -1, lastDigit = -1;    // control number -1, which should not be on the output
+            lastDigit = inNumber % 10;
+            for (int i = 10; i <= (inNumber * 10); i *= 10)
+            {
+                firstDigit = (inNumber % i) / (i / 10);
+            }
+            Console.WriteLine($"Das Produkt der ersten und letzten Ziffer von {inNumber} ist {firstDigit}*{lastDigit}={firstDigit*lastDigit}.");
+            // END task e)
+
+            // ask for any key to exit program
+            Console.WriteLine($"\n");
+            Console.Write("Press any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
